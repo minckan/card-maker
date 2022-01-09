d31c2b04fa9038776b452f091cb41ec08b059bc8
minckan <mymy317@naver.com>
2022-01-09 14:56:47 +0900

    project set up & add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,14 @@
+
+import { database, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL:process.env.REACT_APP_FIREBASE_DB_URL
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
