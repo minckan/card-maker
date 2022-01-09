788bc5fd2346271adb351b96b059c470da3e8d75
minckan <mymy317@naver.com>
2022-01-09 20:22:00 +0900

    add card and edit form component

--- src/Components/maker/maker.jsx
+++ src/Components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
@@ -6,7 +6,12 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 import styles from './maker.module.css'
 
-const Maker = ({authService}) => {
+const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        { id: '1', name: 'mj', company: 'kako',  title: 'Software Engineer',theme: 'light', email: 'minckan@github.com', message: 'go for it!', fileName: 'mmjj', fileUrl: 'mmjj.png'},
+        { id: '2', name: 'mj', company: 'kako',  title: 'Software Engineer',theme: 'colorful', email: 'minckan@github.com', message: 'go for it!', fileName: 'mmjj', fileUrl: 'mmjj.png'},
+        { id: '3', name: 'mj', company: 'kako',  title: 'Software Engineer',theme: 'dark', email: 'minckan@github.com', message: 'go for it!', fileName: 'mmjj', fileUrl: null}
+    ])
     const onLogout = () => {
         authService.logout()
     }
@@ -20,14 +25,14 @@ const Maker = ({authService}) => {
         })
     })
     return (
-            <section className={styles.maker}>
-                <Header onLogout={onLogout} />
-                <div className={styles.container}>
-                    <Editor />
-                    <Preview />
-                </div>
-                <Footer />
-            </section>
+        <section className={styles.maker}>
+            <Header onLogout={onLogout} />
+            <div className={styles.container}>
+                <Editor cards={cards}/>
+                <Preview  cards={cards} />
+            </div>
+            <Footer />
+        </section>
     )
 }
 
