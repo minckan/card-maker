0d5c82dc0dc338ec4eab758e22a6e826c1e164ac
minckan <mymy317@naver.com>
2022-01-09 20:52:31 +0900

    add CardAddForm component

--- src/Components/card_edit_form/card_edit_form.jsx
+++ src/Components/card_edit_form/card_edit_form.jsx
@@ -10,16 +10,16 @@ const CardEditForm = ({ card }) => {
     }
     return (
         <form className={styles.form}>
-            <input className={styles.input} type="text" name='name' value={name}/>
-            <input className={styles.input} type="text" name='company' value={company} />
-            <select  className={styles.select} name="theme" value={theme}>
+            <input className={styles.input} type="text" name='name' defaultValue={name}/>
+            <input className={styles.input} type="text" name='company' defaultValue={company} />
+            <select  className={styles.select} name="theme" defaultValue={theme}>
                 <option value="light">Light</option>
                 <option value="dark">Dark</option>
                 <option value="colorful">Colorful</option>
             </select>
-            <input  className={styles.input} type="text" name='title' value={title}/>
-            <input  className={styles.input} type="text" name='email' value={email} />
-            <textarea className={styles.textarea} name="message" value={message}></textarea>
+            <input  className={styles.input} type="text" name='title' defaultValue={title}/>
+            <input  className={styles.input} type="text" name='email' defaultValue={email} />
+            <textarea className={styles.textarea} name="message" defaultValue={message}></textarea>
             <div  className={styles.fileInput}>
                 <ImageFileInput></ImageFileInput>
             </div>
--- src/Components/editor/editor.jsx
+++ src/Components/editor/editor.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import CardAddForm from '../card_add_form/card_add_form';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = ({cards}) => (
+const Editor = ({cards, addCard}) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
-    <ul>
     {cards.map(card => <CardEditForm card={card} key={card.id}/>)}
-    </ul>
+    <CardAddForm onAdd={addCard}></CardAddForm>
   </section>
 );
 
